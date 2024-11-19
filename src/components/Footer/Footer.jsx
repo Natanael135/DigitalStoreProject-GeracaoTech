4e2ee062b8428bed7211eb72e752cac13b9551ee
Erik Lima <eriklima832@gmail.com>
2024-11-19 00:44:33 -0300

    git commit -m "adicionado Esqueleto do Componente Footer"

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,32 @@
+import Logo from "../Logo/Logo";
+
 const Footer = () => {
-  return (  
-    <>
-    <h1>Footer teste</h1>
-    </>
-  );
-}
+  const infoLinks = [
+      {
+        title: "Informação",
+        links: [
+          { label: "Sobre Drip Store", path: "/Error" },
+          { label: "Segurança", path: "/Error" },
+          { label: "Wishlist", path: "/Error" },
+          { label: "Blog", path: "/Error" },
+          { label: "Trabalhe conosco", path: "/Error" },
+          { label: "Meus Pedidos", path: "/Pedidos" },
+        ],
+      },
+      {
+        title: "Categorias",
+        links: [
+          { label: "Camisetas", path: "/ProductList" },
+          { label: "Calças", path: "/ProductList" },
+          { label: "Bonés", path: "/ProductList" },
+          { label: "Headphones", path: "/ProductList" },
+          { label: "Tênis", path: "/ProductList" },
+        ],
+      },
+    ]
+  return (
+    Footer
+  )
+};
  
 export default Footer;
