67efd6308d00dc83afbc8af75c04b03439e5d8c3
Matheus Ferreira <Matheusferreirasilvarocha@gmail.com>
2024-11-25 12:30:44 -0300

    feat: primeiro banner do carrossel

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -12,6 +12,23 @@ import ProductCard from "../../components/ProductCard/card";
 import ProductListing from "../../components/ProductListing/ProductListing";
 import products from "../../components/products/products.json"
 import FeaturedProducts from "../../components/FeaturedProducts"
+import banner from "../../assets/banner/white-sneaker-nike.png"
+
+const LoginButton = styled.button`
+  color: white;
+  background-color: #c92071;
+  border: 0;
+  border-radius: 8px;
+  padding: 10px 20px;
+
+  &:hover {
+    background-color: #991956;
+  }
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
 
 const HomeContainer = styled.main`
 background-color: #F9F8FE;
@@ -26,10 +43,65 @@ background-color: #F9F8FE;
       border-radius: 8px;
     }
 }
-
-  & #banner1, #banner2, #banner3{
+  & #carouselExampleCaptions{
+    height:581px;
     background-color: var(--light-gray-3);
-    height:681px;
+    & #banner{
+      padding: 30px 30px ;
+
+      & .bannerContent{
+        margin: auto;
+        width: 85%;
+        height: 100%;
+        border: 1px solid red;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-flow: row-reverse wrap;
+        @media (min-width: 1024px){
+          height: 398px;
+        }
+        & #bannerInfos{
+          width: 80%;
+          height: 50%;
+          @media (min-width:1024px){
+            width: 50%;
+            height: 100%;
+
+          }
+        }
+        & #bannerImage{
+          position: relative;
+          width: 80%;
+          height: 50%;
+          border: 1px solid red;
+          @media (min-width:1024px){
+            width: 50%;
+            height: 100%;
+          }
+          & img{
+            position:absolute ;
+            width: 460px;
+            height: 400px;
+            object-fit: contain;
+            transform: rotate(-19.34deg);
+            
+            @media (min-width: 1024px){
+              right: -2%;
+              top: -30%;
+              width: 120%;
+              height: 150%;
+              @media (min-width: 1400px){
+                width: 150%;
+                height: 150%;
+                right: -20%;
+                top: -35%;
+              }
+          }
+        }
+        }
+      }
+    }
   }
   & .main-content{
     padding:30px 50px;
@@ -49,22 +121,33 @@ const HomePage = () => {
         </div>
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <div id="banner1">
-              <img src={banner1} className="d-block w-100" alt="..."/>
+            <div id="banner">
+              <div className="bannerContent">
+                <div id="bannerImage">
+                  <img src={banner} alt="" />
+                </div>
+                <div id="bannerInfos">
+                  <h6>Melhores ofertas personalizadas</h6>
+                  <h1>Queima de estoque Nike</h1>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quisquam exercitationem doloribus </p>
+                  <LoginButton>Entrar</LoginButton>
+                </div>
+              </div>
+
             </div>
           </div>
           <div className="carousel-item">
-            <div id="banner2">
+            <div id="banner">
               <img src="..." className="d-block w-100" alt="..."/>
             </div>
           </div>
           <div className="carousel-item">
-            <div id="banner2">
+            <div id="banner">
               <img src="..." className="d-block w-100" alt="..."/>
             </div>
           </div>
           <div className="carousel-item">
-            <div id="banner2">
+            <div id="banner">
               <img src="..." className="d-block w-100" alt="..."/>
             </div>
           </div>
